fb8f848b218daf684ef22b5b74e94130e435ca18
Abstract Void <pacodejesus2006@gmail.com>
2026-09-12 07:58:33 -0600

    changed so that the login component works

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Dashboard from './components/DashBoard.jsx'
+import Login from './components/Login.jsx'
+import './components/Login.css'
 import { initialCompanies, initialMovements } from './components/data.js'
 
 function App() {
   // Vista de prueba: el dashboard recibe datos locales, no una sesión bancaria.
-  return <Dashboard company={initialCompanies[0]} movements={initialMovements} />
+  return <Login  />
 }
 
 export default App
